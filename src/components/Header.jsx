0f19378fc5dd1da992cc7d802e1fc42393e9cb5d
Adel <leda91@hotmail.fr>
2024-03-25 23:08:25 +0100

    header file added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,62 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { Link as ScrollLink } from 'react-scroll';
+import { useState } from 'react';
 
-function Header() {
+const Header = () => {
+  //States
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  //Comportement
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  //Render
   return (
-    <div>
-      <h1> Mon restaurant</h1>
-    </div>
+    <header className="header">
+      <div className="header-content">
+        <a className="AdelDev" href='#whoIamSection'>
+          <p className="name">Adel Drissi</p>
+        </a>
+        <div
+          className={`anchorLinks ${
+            isMenuOpen ? 'anchorLinks--open AppearLink' : ''
+          }`}
+        >
+          <ul className="anchorLinks__list">
+            <li onClick={() => setIsMenuOpen(false)}>
+              <ScrollLink to="homeSection" smooth={true} duration={700}>
+                Accueil
+              </ScrollLink>
+            </li>
+            <li onClick={() => setIsMenuOpen(false)}>
+              <ScrollLink to="aboutSection" smooth={true} duration={700}>
+                A propos
+              </ScrollLink>
+            </li>
+            <li onClick={() => setIsMenuOpen(false)}>
+              <ScrollLink to="projectSection"  smooth={true} duration={700}>
+                Nos services
+              </ScrollLink>
+            </li>
+            <li onClick={() => setIsMenuOpen(false)}>
+              <ScrollLink to="sectionContact" smooth={true} duration={700}>
+                Contact
+              </ScrollLink>
+            </li>
+          </ul>
+        </div>
+        {/* <div className="IconResponsive">
+          <FontAwesomeIcon
+            icon={faBars}
+            className="NavbarIcons"
+            onClick={toggleMenu}
+            aria-label="menu de navigation"
+          />
+        </div> */}
+      </div>
+    </header>
   );
-}
-
+};
 export default Header;
