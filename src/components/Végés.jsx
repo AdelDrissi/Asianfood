04b4d35a5bbb33fb7c7fe88b5068adef63688078
Adel <leda91@hotmail.fr>
2024-05-02 18:48:25 +0200

    section végé modified

--- src/components/Végés.jsx
+++ src/components/Végés.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Végétal from '../images/Végétal.jpg';
 
-function Lunch() {
+function Végés() {
   return (
     <section className="végés-container">
       <div className="végés-japanese">
         <div className="image-container-végés">
-          <img src={Végétal} alt="Background" className="lunch-box" />
+          <img src={Végétal} alt="Background" className="végés-box" />
           <div className="végés-overlay">
-            <h1 className="lunch-title"> Menus Végés! </h1>
+            <h1 className="lunch-title"> Lunch Box ! </h1>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
               reprehenderit ex aperiam officiis suscipit aliquam iusto, quam
@@ -24,4 +24,4 @@ function Lunch() {
   );
 }
 
-export default Lunch;
+export default Végés;
